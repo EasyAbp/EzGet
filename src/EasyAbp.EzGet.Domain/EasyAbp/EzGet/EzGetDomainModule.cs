a099b50d9e19f2cd12ea69944ef4c982a738d90a
SayHelloGsx <550800945@qq.com>
2021-10-21 18:27:16 +0800

    Add abp blob storing module

--- src/EasyAbp.EzGet.Domain/EasyAbp/EzGet/EzGetDomainModule.cs
+++ src/EasyAbp.EzGet.Domain/EasyAbp/EzGet/EzGetDomainModule.cs
@@ -1,12 +1,14 @@
 ﻿using Volo.Abp.Domain;
 using Volo.Abp.Modularity;
 using Volo.Abp.Users;
+using Volo.Abp.BlobStoring;
 
 namespace EasyAbp.EzGet
 {
     [DependsOn(
         typeof(AbpDddDomainModule),
         typeof(AbpUsersDomainModule),
+        typeof(AbpBlobStoringModule),
         typeof(EzGetDomainSharedModule)
     )]
     public class EzGetDomainModule : AbpModule
