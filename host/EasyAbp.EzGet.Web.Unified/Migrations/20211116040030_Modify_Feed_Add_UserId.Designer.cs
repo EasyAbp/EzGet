0e6cdf7e4b75e5b962339826b89940251ee4e2cf
SayHelloGsx <550800945@qq.com>
2021-11-16 12:01:06 +0800

    Add migration

--- host/EasyAbp.EzGet.Web.Unified/Migrations/20211116040030_Modify_Feed_Add_UserId.Designer.cs
+++ host/EasyAbp.EzGet.Web.Unified/Migrations/20211116040030_Modify_Feed_Add_UserId.Designer.cs
@@ -4,15 +4,17 @@ using EasyAbp.EzGet.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
 namespace EasyAbp.EzGet.Migrations
 {
     [DbContext(typeof(UnifiedDbContext))]
-    partial class UnifiedDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211116040030_Modify_Feed_Add_UserId")]
+    partial class Modify_Feed_Add_UserId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ host/EasyAbp.EzGet.Web.Unified/Migrations/20211116040030_Modify_Feed_Add_UserId.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EasyAbp.EzGet.Migrations
+{
+    public partial class Modify_Feed_Add_UserId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "EzGetFeeds",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "EzGetFeeds");
+        }
+    }
+}
