2bdd65576e483f3dc855477a8d730a1449d837d1
SayHelloGsx <550800945@qq.com>
2021-11-18 13:54:01 +0800

    Add CredentialPublicController

---
+++ src/EasyAbp.EzGet.Public.HttpApi/EasyAbp/EzGet/Public/Credentials/CredentialPublicController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+
+namespace EasyAbp.EzGet.Public.Credentials
+{
+    [RemoteService(Name = EzGetPublicRemoteServiceConsts.RemoteServiceName)]
+    [Area("ezget-public")]
+    [Route("api/ezget-public/credentials")]
+    public class CredentialPublicController : EzGetPublicControllerBase, ICredentialPublicAppService
+    {
+        protected ICredentialPublicAppService CredentialPublicAppService { get; }
+
+        public CredentialPublicController(ICredentialPublicAppService credentialPublicAppService)
+        {
+            CredentialPublicAppService = credentialPublicAppService;
+        }
+
+        [HttpPost]
+        public virtual Task<CredentialDto> CreateAsync(CreateCredentialDto input)
+        {
+            return CredentialPublicAppService.CreateAsync(input);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public virtual Task DeleteAsync(Guid id)
+        {
+            return CredentialPublicAppService.DeleteAsync(id);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public virtual Task<CredentialDto> GetAsync(Guid id)
+        {
+            return CredentialPublicAppService.GetAsync(id);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public virtual Task<CredentialDto> UpdateAsync(Guid id, UpdateCredentialDto inpu)
+        {
+            return CredentialPublicAppService.UpdateAsync(id, inpu);
+        }
+    }
+}
