43cfb8974fb7f9316e0bba483b18a4dc40a61b6e
SayHelloGsx <550800945@qq.com>
2021-11-18 13:45:15 +0800

    Add FeedPublicController

--- src/EasyAbp.EzGet.Public.Application.Contracts/EasyAbp/EzGet/Public/Feeds/IFeedPublicAppService.cs
+++ src/EasyAbp.EzGet.Public.Application.Contracts/EasyAbp/EzGet/Public/Feeds/IFeedPublicAppService.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using EasyAbp.EzGet.Feeds;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 
 namespace EasyAbp.EzGet.Public.Feeds
 {
     public interface IFeedPublicAppService : IApplicationService
     {
+        Task<FeedDto> GetAsync(Guid id);
+        Task<PagedResultDto<FeedDto>> GetListAsync(GetFeedsInput input);
+        Task<FeedDto> CreateAsync(CreateFeedPublicDto input);
+        Task<FeedDto> UpdateAsync(Guid id, UpdateFeedPublicDto input);
+        Task DeleteAsync(Guid id);
     }
 }
---
+++ src/EasyAbp.EzGet.Public.HttpApi/EasyAbp/EzGet/Public/Feeds/FeedPublicController.cs
@@ -0,0 +1,58 @@
+﻿using EasyAbp.EzGet.Feeds;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Application.Dtos;
+
+namespace EasyAbp.EzGet.Public.Feeds
+{
+    [RemoteService(Name = EzGetPublicRemoteServiceConsts.RemoteServiceName)]
+    [Area("ezget-public")]
+    [Route("api/ezget-public/feeds")]
+    public class FeedPublicController : EzGetPublicControllerBase, IFeedPublicAppService
+    {
+        protected IFeedPublicAppService FeedPublicAppService { get; }
+
+        public FeedPublicController(IFeedPublicAppService feedPublicAppService)
+        {
+            FeedPublicAppService = feedPublicAppService;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public virtual Task<FeedDto> GetAsync(Guid id)
+        {
+            return FeedPublicAppService.GetAsync(id);
+        }
+
+        [HttpGet]
+        public virtual Task<PagedResultDto<FeedDto>> GetListAsync(GetFeedsInput input)
+        {
+            return FeedPublicAppService.GetListAsync(input);
+        }
+
+        [HttpPost]
+        public virtual Task<FeedDto> CreateAsync(CreateFeedPublicDto input)
+        {
+            return FeedPublicAppService.CreateAsync(input);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public virtual Task<FeedDto> UpdateAsync(Guid id, UpdateFeedPublicDto input)
+        {
+            return FeedPublicAppService.UpdateAsync(id, input);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public virtual Task DeleteAsync(Guid id)
+        {
+            return FeedPublicAppService.DeleteAsync(id);
+        }
+    }
+}
